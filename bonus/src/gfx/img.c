20ef53734b351c9e3c0a4ffae98b5e8f9e47867b
dath <tjouvenc@3D3.42angouleme.fr>
2025-05-05 18:40:13 +0200

    optimize clear_img when color is 0

--- bonus/src/gfx/img.c
+++ bonus/src/gfx/img.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   img.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dath <dath@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: tjouvenc <tjouvenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 19:50:44 by tjouvenc          #+#    #+#             */
-/*   Updated: 2025/04/26 18:13:39 by dath             ###   ########.fr       */
+/*   Updated: 2025/05/05 18:39:33 by tjouvenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "gfx.h"
+#include <string.h>
 
 int	img_init(t_gfx *gfx, t_img *img, t_point size)
 {
@@ -38,6 +39,11 @@ void	clear_img(t_img *img, t_color clear_color)
 	t_color	*data;
 
 	data = img->data;
+	if (!clear_color)
+	{
+		memset(data, 0, img->size.x * img->size.y);
+		return ;
+	}
 	y = img->size.y;
 	while (--y >= 0)
 	{
