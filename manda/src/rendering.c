9c26a94c30742bbf16b670c420a882c5ba29e4f4
Lucien <lguerbig@3A6.42angouleme.fr>
2025-05-06 11:14:06 +0200

    FIX: shift in rendering

--- manda/src/rendering.c
+++ manda/src/rendering.c
@@ -6,7 +6,7 @@
 /*   By: lguerbig <lguerbig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 18:54:13 by tjouvenc          #+#    #+#             */
-/*   Updated: 2025/05/05 18:30:11 by lguerbig         ###   ########.fr       */
+/*   Updated: 2025/05/06 11:12:33 by lguerbig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,7 +127,7 @@ void	render(t_gfx *gfx)
 	while (screen_coord_x < WIN_X)
 	{
 		angle_x = gfx->player.dir + atanf((screen_coord_x
-					- WIN_Y) / (gfx->screen_dist * WIN_X));
+					- WIN_X / 2) / (gfx->screen_dist * WIN_X));
 		dir = (t_vec2){{cosf(angle_x), sinf(angle_x)}};
 		rc = raycast(gfx->player.pos, dir, gfx, gfx->max_raycast_distance);
 		if (isnan(rc.distance))
